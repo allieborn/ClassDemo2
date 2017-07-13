b335b80d1d01f580aed89d5c0206d615e587d17c
Alexandra <aborn601722@Alexandras-MacBook-Pro.local>
2017-07-13 09:23:51 -0400

    in class demo

---
+++ src/com/GrandCircus/Main.java
@@ -0,0 +1,8 @@
+package com.GrandCircus;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("This is an in class demo on pushing to GitHub.");
+    }
+}
